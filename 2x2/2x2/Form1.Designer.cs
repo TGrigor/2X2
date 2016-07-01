95f0b06bb6892514b508e5ac95d1c8b3c21829d8
Grigor Temejikyan <Grigor Temejikyan>
2016-07-01 17:31:24 +0400

    first push

---
+++ 2x2/2x2/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace _2x2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.TextOut = new System.Windows.Forms.TextBox();
+            this.OK = new System.Windows.Forms.Button();
+            this.TextInp = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // TextOut
+            // 
+            this.TextOut.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.TextOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TextOut.ForeColor = System.Drawing.Color.Black;
+            this.TextOut.Location = new System.Drawing.Point(3, 98);
+            this.TextOut.Multiline = true;
+            this.TextOut.Name = "TextOut";
+            this.TextOut.Size = new System.Drawing.Size(138, 49);
+            this.TextOut.TabIndex = 0;
+            this.TextOut.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.TextOut.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.TextOut.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // OK
+            // 
+            this.OK.Location = new System.Drawing.Point(3, 188);
+            this.OK.Name = "OK";
+            this.OK.Size = new System.Drawing.Size(189, 38);
+            this.OK.TabIndex = 1;
+            this.OK.Text = "Հաստատել";
+            this.OK.UseVisualStyleBackColor = true;
+            this.OK.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TextInp
+            // 
+            this.TextInp.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.TextInp.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TextInp.ForeColor = System.Drawing.Color.Black;
+            this.TextInp.Location = new System.Drawing.Point(147, 98);
+            this.TextInp.Multiline = true;
+            this.TextInp.Name = "TextInp";
+            this.TextInp.Size = new System.Drawing.Size(45, 48);
+            this.TextInp.TabIndex = 2;
+            this.TextInp.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.TextInp.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            this.TextInp.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 153);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(189, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_2);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress_2);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DodgerBlue;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(196, 283);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.TextInp);
+            this.Controls.Add(this.OK);
+            this.Controls.Add(this.TextOut);
+            this.Cursor = System.Windows.Forms.Cursors.PanNW;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.RightToLeftLayout = true;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "2*2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextOut;
+        private System.Windows.Forms.Button OK;
+        private System.Windows.Forms.TextBox TextInp;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ 2x2/2x2/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2x2
+{
+    public partial class Form1 : Form
+    {
+        static Random rand = new Random();
+        private int x = 0;
+        private int y = 0;
+        private int z = 0;
+        private int point = 0;
+        private int counter = 0;
+        private string Humenname ;
+        public void NewRandInic()
+        {
+            x = rand.Next(0, 10);
+            y = rand.Next(0, 10);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            NewRandInic();
+            text();
+            textBox1.Text = "Ձեր Միավորը- --" ;
+        }
+        public void text()
+        {
+            TextOut.Text = x + " * " + y + "= ";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try {
+                z = Int32.Parse(TextInp.Text);
+                counter++;                
+                if (counter != 20)
+                {
+                    if (x * y == z)
+                    {
+                        point++;
+                        textBox1.Text = "Ձեր Միավորը "+ point.ToString();
+                        TextOut.BackColor = Color.LightSeaGreen;
+                        TextInp.BackColor = Color.LightSeaGreen;
+                        MessageBox.Show("Ճիշտ է ))");
+                        NewRandInic();
+                        text();
+                    }
+                    else
+                    {
+                        point--;
+                        textBox1.Text = "Ձեր Միավորը " + point.ToString();
+                        TextOut.BackColor = Color.Red;
+                        TextInp.BackColor = Color.Red;
+                        MessageBox.Show("Սխալ է եղեք ավելի ուշադիր ․․․");
+                        text();
+                    }
+                }
+                else
+                {
+                    counter = 0;
+                    if (point>=10 && point<=15)
+                    {
+                        MessageBox.Show("Խաղն ավարտվեց ձեր միավորը-" + point+" Կրկնեք բազմապատկման աղյուսյակը ;)");
+                    }
+                    else if (point>15 && point<20)
+                    {
+                        MessageBox.Show("Խաղն ավարտվեց ձեր միավորը-" + point + " Դուք լավ գիտեք բազմապատկման աղյուսյակու ․Շարունակեք նույն տեմպերով ;)");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Ամոթ՝՝՝   նույնիսկ չեմ ուզում նայել ձեր միավորին․․․․ սովորեք բազմապատկման աղյուսյակը  ");
+                    }
+                    point = 0;
+                }
+                 }
+            catch
+            {
+            }
+        }
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {           
+        }
+
+        private void textBox1_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+            if (!Char.IsDigit(ch) && ch != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode==Keys.Enter)
+            {
+                OK.Click += button1_Click;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyPress_2(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_2(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
